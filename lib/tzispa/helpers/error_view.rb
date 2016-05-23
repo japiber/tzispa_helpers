05edc3d0a05aaf4db1fd9a9f7da4848f757f77a7
Juan Antonio Piñero <japinero@area-integral.com>
2016-05-23 15:16:04 +0200

    rename error_report to debug_info

--- lib/tzispa/helpers/error_view.rb
+++ lib/tzispa/helpers/error_view.rb
@@ -1,25 +1,27 @@
+require 'tzispa'
 require 'tzispa/rig/template'
-
 
 module Tzispa
   module Helpers
     module ErrorView
 
-      def error_report(error=nil)
-        text = String.new('<!DOCTYPE html>')
-        text << '<html lang="es"><head>'
-        text << '<meta charset="utf-8" />'
-        text << '<style> html {background:#cccccc; font-family:Arial; font-size:15px; color:#555;} body {width:75%; max-width:1200px; margin:18px auto; background:#fff; border-radius:6px; padding:32px 24px;} ul{list-style:none; margin:0; padding:0;} li{font-style:italic; color:#666;} h1 {color:#2ECC71;} </style>'
-        text << '</head><body>'
-        text << "<h5>#{Tzispa::FRAMEWORK_NAME} #{Tzispa::VERSION}</h5>\n"
-        if error
-          text << "<h1>#{error.class.name}</h1><h3>#{error.message}</h1>\n"
-          text << '<ol>' + error.backtrace.map { |trace| "<li>#{trace}</li>\n" }.join + '</ol>' if error.respond_to?(:backtrace) && error.backtrace
-        else
-          text << "<h1>Error 500</h1>\n"
-          text << "Se ha producido un error inesperado al tramitar la petición"
-        end
-        text << '</body></html>'
+      def debug_info(exception=nil, status: 500)
+        String.new.tap { |text|
+          text << '<!DOCTYPE html>'
+          text << '<html lang="es"><head>'
+          text << '<meta charset="utf-8" />'
+          text << '<style> html {background:#cccccc; font-family:Arial; font-size:15px; color:#555;} body {width:75%; max-width:1200px; margin:18px auto; background:#fff; border-radius:6px; padding:32px 24px;} ul{list-style:none; margin:0; padding:0;} li{font-style:italic; color:#666;} h1 {color:#2ECC71;} </style>'
+          text << '</head><body>'
+          text << "<h5>#{Tzispa::FRAMEWORK_NAME} #{Tzispa::VERSION}</h5>\n"
+          if exception
+            text << "<h1>#{exception.class.name}</h1><h3>#{exception.message}</h1>\n"
+            text << '<ol>' + exception.backtrace.map { |trace| "<li>#{trace}</li>\n" }.join + '</ol>' if exception.respond_to?(:backtrace) && exception.backtrace
+          else
+            text << "<h1>Error #{status}</h1>\n"
+            text << "Se ha producido un error inesperado al tramitar la petición"
+          end
+          text << '</body></html>'
+        }
       end
 
       def error_page(domain, status: 500)
@@ -27,16 +29,18 @@ module Tzispa
           error_file = "#{domain.path}/error/#{status}.htm"
           Tzispa::Rig::File.new(error_file).load!.content
         rescue
-          text = String.new('<!DOCTYPE html>')
-          text << '<html lang="es"><head>'
-          text << '<meta charset="utf-8" />'
-          text << '<style> html {background:#cccccc; font-family:Arial; font-size:15px; color:#555;} body {width:75%; max-width:1200px; margin:18px auto; background:#fff; border-radius:6px; padding:32px 24px;} #main {margin:auto; } h1 {color:#2ECC71; font-size:4em; text-align:center;} </style>'
-          text << '</head><body>'
-          text << '<div id="main">'
-          text << "<h5>#{Tzispa::FRAMEWORK_NAME} #{Tzispa::VERSION}</h5>\n"
-          text << "<h1>Error #{status}</h1>\n"
-          text << '</div>'
-          text << '</body></html>'
+          String.new.tap { |text|
+            text << '<!DOCTYPE html>'
+            text << '<html lang="es"><head>'
+            text << '<meta charset="utf-8" />'
+            text << '<style> html {background:#cccccc; font-family:Arial; font-size:15px; color:#555;} body {width:75%; max-width:1200px; margin:18px auto; background:#fff; border-radius:6px; padding:32px 24px;} #main {margin:auto; } h1 {color:#2ECC71; font-size:4em; text-align:center;} </style>'
+            text << '</head><body>'
+            text << '<div id="main">'
+            text << "<h5>#{Tzispa::FRAMEWORK_NAME} #{Tzispa::VERSION}</h5>\n"
+            text << "<h1>Error #{status}</h1>\n"
+            text << '</div>'
+            text << '</body></html>'
+          }
         end
       end
 
