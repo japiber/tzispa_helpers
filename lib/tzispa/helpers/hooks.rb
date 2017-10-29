d8432858774d90b76d74cddb915aae62cadf0714
Juan Antonio Piñero <japinero@area-integral.com>
2017-10-29 17:30:51 +0100

    added hooks helper module

---
+++ lib/tzispa/helpers/hooks.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Tzispa
+  module Helpers
+    module Hooks
+
+      autoload :After,    'tzispa/helpers/hooks/after'
+      autoload :Before,   'tzispa/helpers/hooks/before'
+
+    end
+  end
+end
